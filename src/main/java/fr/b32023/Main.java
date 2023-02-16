651773b142616783c4fdc7aba1088fa0457007a4
Maylis Le Borgne <maylislb@hotmail.fr>
2023-02-16 12:05:54 +0100

    Initial commit

---
+++ src/main/java/fr/b32023/Main.java
@@ -0,0 +1,7 @@
+package fr.b32023;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/fr/b32023/entities/Fish.java
@@ -0,0 +1,4 @@
+package fr.b32023.entities;
+
+public class Fish {
+}
